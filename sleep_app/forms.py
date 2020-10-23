449da86cace37732964399fd3d475e4ee9fafc98
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-10-23 16:29:34 +0100

    Create the corresponding forms for each of the Response subclasses.

---
+++ sleep_app/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from sleep_app.models import Person, Symptom, YesNoResponse, TextResponse, ScaleResponse
+
+
+class YesNoResponseForm(forms.ModelForm):
+#   needs to be required=False because the answer can be yes or no
+    answer = forms.BooleanField(widget=forms.NullBooleanSelect, required=False)
+
+    class Meta:
+        model = YesNoResponse
+        fields = ('answer',)
+
+
+class TextResponseForm(forms.ModelForm):
+    answer = forms.CharField()
+
+    class Meta:
+        model = TextResponse
+        fields = ('answer',)
+
+
+class ScaleResponseForm(forms.ModelForm):
+    answer = forms.IntegerField()
+
+    class Meta:
+        model = ScaleResponse
+        fields = ('answer',)
