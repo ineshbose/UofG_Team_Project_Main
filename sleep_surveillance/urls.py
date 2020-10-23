77b4e2096cd5accffdec4760c09f86ba5f1c599a
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-10-23 16:28:15 +0100

    set up urls.py in sleep_app/ and sleep_surveillance/. Supports /admin, /form, /form/<symptom-slug>, and media urls.

--- sleep_surveillance/urls.py
+++ sleep_surveillance/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sleep_app import urls
+from django.urls import include
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('sleep_app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
