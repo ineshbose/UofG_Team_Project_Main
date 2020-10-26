2ed3f9663707efba7fc8f87d0df5b02d81dee233
Jingqi Li <2431148l@student.gla.ac.uk>
2020-10-26 16:02:54 +0000

    Adding map url

--- sleep_app/urls.py
+++ sleep_app/urls.py
@@ -7,5 +7,6 @@ app_name = 'sleep_app'
 urlpatterns = [
     path('form/<slug:symptom_name_slug>/',
          views.symptom_question, name='symptom form'),
-    path('form/', views.form, name='main form page')
+    path('form/', views.form, name='main form page'),
+    path('map/',views.map, name='map'),
 ]
