8dea0bf54b7990a8cbeac8712b6174e7ecf72eab
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-10-28 13:49:35 +0000

    Add a counter to show how many forms the user has filled out in the last 24h.

--- sleep_app/views.py
+++ sleep_app/views.py
@@ -1,26 +1,49 @@
 from django.shortcuts import render
 from sleep_app.models import Symptom, Person, Response, YesNoResponse, TextResponse, ScaleResponse
 from sleep_app.forms import YesNoResponseForm, TextResponseForm, ScaleResponseForm
+import random
+import datetime
 from django.shortcuts import redirect
 from django.http import HttpResponse
-# Create your views here.
 
-#the view for the page that takes you to the questions
+
+# helper function. Generates a person object with a unique random id whenever the first page of a question is visited.
+def create_person_and_id(request):
+    new_id = int(random.uniform(0, 1000000))
+#   to prevent id collision
+    while request.session.get('person') == new_id:
+        new_id = int(random.uniform(0, 1000000))
+    person = Person(id=new_id)
+    person.save()
+    request.session['person'] = person.id
+
+
+def increase_log_amount(request):
+    request.session['log_amount'] = request.session.get('log_amount', 0) + 1
+
+
+# Create your views here.
+# the view for the page that takes you to the questions
 def form(request):
     context_dict = {}
 #   this is fine for testing, but should be replaced by something more stable
-    first_symptom = Symptom.objects.get(pk=1)
+    first_symptom = Symptom.objects.first()
     context_dict['first_symptom'] = first_symptom
-#   set a cookie, so that all responses can be associated with one person
-#   this is fine for testing, but should be replaced by something more stable
-    if 'person' not in request.session.keys():
-        person = Person()
-        person.save()
-        request.session['person'] = person.pk
+
+#   this means that that the session will expire in *24h*, not at the beginning of the next day
+#   1209600 is the default return value
+#   (https://docs.djangoproject.com/en/3.1/ref/settings/#std:setting-SESSION_COOKIE_AGE)
+    if request.session.get_expiry_age() == 1209600:
+        request.session.set_expiry(60*60*24)
+    create_person_and_id(request)
+    print("exp: {age}".format(age=request.session.get_expiry_age()))
+    context_dict['log_amount'] = request.session.get('log_amount', 0)
+
+    print("Person id is {id}".format(id = request.session['person']))
     return render(request, 'sleep_app/form.html', context_dict)
 
 
-#the view for the page that asks about a specific symptom
+# the view for the page that asks about a specific symptom
 def symptom_question(request, symptom_name_slug):
     context_dict = {}
     try:
@@ -28,20 +51,14 @@ def symptom_question(request, symptom_name_slug):
         context_dict['symptom'] = symptom
 #       need to pass the proper type of response object to the template, depending on what type of response is needed
         if symptom.answer_type == 'bool':
-            response = YesNoResponse(symptom=symptom)
-            context_dict['response'] = response
             response_form = YesNoResponseForm()
         elif symptom.answer_type == 'text':
-            response = TextResponse(symptom=symptom)
-            context_dict['response'] = response
             response_form = TextResponseForm()
         else:
-            response = ScaleResponse(symptom=symptom)
-            context_dict['response'] = response
             response_form = ScaleResponseForm()
         context_dict['response_form'] = response_form
     except Symptom.DoesNotExist:
-        context_dict['symptom'] = context_dict['response'] = context_dict['response_form'] = None
+        context_dict['symptom'] = context_dict['response_form'] = None
 
     if request.method == 'POST':
         try:
@@ -68,18 +85,29 @@ def symptom_question(request, symptom_name_slug):
             return redirect('/form')
 
         try:
-            current_person = Person.objects.get(pk=request.session['person'])
+            current_person = Person.objects.get(id=request.session['person'])
             current_person.response.add(response)
             current_person.save()
+
         except Person.DoesNotExist:
+            print("person does not exist")
             return redirect('/form')
 
 #       this is probably NOT a good way of getting to the next symptom!
         try:
-            next_symptom = Symptom.objects.get(pk=symptom.pk + 1)
-            return redirect('/form/{slug}'.format(slug=next_symptom.slug))
+            if symptom == Symptom.objects.last():
+                increase_log_amount(request)
+                return redirect('/form')
+            else:
+                next_symptom = None
+                key = symptom.pk
+                while not next_symptom:
+                    key = key + 1
+                    next_symptom = Symptom.objects.filter(pk=key).first()
+                return redirect('/form/{slug}'.format(slug=next_symptom.slug))
 
         except Symptom.DoesNotExist:
+            print("symptom does not exist")
             context_dict['symptom'] = context_dict['response'] = context_dict['response_form'] = None
             return redirect('/form')
 
