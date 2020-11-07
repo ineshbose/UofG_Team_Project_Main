d6ef4be01304e7ce2d92194b87132ce6af674b7c
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-11-07 11:51:39 +0000

    remove spaces in url names

--- sleep_app/urls.py
+++ sleep_app/urls.py
@@ -6,6 +6,7 @@ app_name = 'sleep_app'
 
 urlpatterns = [
     path('form/<slug:symptom_name_slug>/',
-         views.symptom_question, name='symptom form'),
-    path('form/', views.form, name='main form page')
+         views.symptom_question, name='symptom_form'),
+    path('form/', views.form, name='main_form_page'),
+    path('location/', views.location, name='location')
 ]
