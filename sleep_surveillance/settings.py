7bb33b6555d0e06604a2841c72aefe3802c5f302
Inesh Bose <2504266b@student.gla.ac.uk>
2020-11-23 18:19:37 +0000

    Fix?

--- sleep_surveillance/settings.py
+++ sleep_surveillance/settings.py
@@ -19,9 +19,6 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = "/media/"
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -159,5 +156,7 @@ PLOTLY_COMPONENT = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
